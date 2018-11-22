0f8792e0a736098cdfd7c9c604e9b8153e559363
sakura <element.kgo@gmail.com>
2018-11-22 05:55:40 +0000

    v5

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.urls import reverse
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 
+
+def post_image_path(instance, filename):
+    return 'posts/{}/{}'.format(instance.pk, filename)
+
+
 class Post(models.Model):
-    image = models.ImageField()
+    image = ProcessedImageField(
+                                    upload_to=post_image_path,
+                                    processors=[ResizeToFill(300,300)],
+                                    format="JPEG",
+                                    options={'quality':90},
+                                )
+
     content = models.TextField()
     
     def get_absolute_url(self):
